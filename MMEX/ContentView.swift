5e42f4da1454b25cde8cdf998c9a1001b676bd9e
Lisheng Guan <guanlisheng@gmail.com>
2024-09-05 13:11:53 +0800

    Initial Commit

---
+++ MMEX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MMEX
+//
+//  Created by Lisheng Guan on 2024/9/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MMEX/MMEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MMEXApp.swift
+//  MMEX
+//
+//  Created by Lisheng Guan on 2024/9/5.
+//
+
+import SwiftUI
+
+@main
+struct MMEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
