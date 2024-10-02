1a69d3b7e5995dde9b6483921f7f302184eda105
George Ef <george.a.ef@gmail.com>
2024-10-02 13:00:59 +0200

    draft for style

--- MMEX/Views/Currencies/CurrencyDetailView.swift
+++ MMEX/Views/Currencies/CurrencyDetailView.swift
@@ -7,6 +7,35 @@
 
 import SwiftUI
 
+struct DetailFieldView<Content: View>: View {
+    let edit  : Bool
+    let label : String
+    var textField: () -> Content
+    //@Binding var value: Value
+
+    init(edit: Bool = false, label: String, @ViewBuilder textField: @escaping () -> Content) {
+        self.edit   = edit
+        self.label  = label
+        self.textField = textField
+    }
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4.0) {
+            Text(label)
+                .font(.body.smallCaps())
+                .fontWeight(.thin)
+                .dynamicTypeSize(.small)
+                .padding(0)
+//            TextField(empty, value: $value, format: .)
+            textField()
+                .padding(.top, 0.0)
+                .padding(.bottom, 0.0)
+                .disabled(!edit)
+        }
+        .padding(0)
+    }
+}
+
 struct CurrencyDetailView: View {
     @EnvironmentObject var dataManager: DataManager // Access DataManager from environment
     @State var currency: CurrencyData
@@ -15,7 +44,77 @@ struct CurrencyDetailView: View {
     @State private var isPresentingEditView = false
     @Environment(\.presentationMode) var presentationMode
 
+    @State var edit: Bool = false
+    @State var format: String = ""
+
     var body: some View {
+        return Form {
+            Section(header: Text("Currency").font(.body)) {
+                DetailFieldView(edit: edit, label: "Name") {
+                    TextField("Currency Name", text: $currency.name)
+                }
+                DetailFieldView(edit: edit, label: "Symbol") {
+                    TextField("Currency Symbol", text: $currency.symbol)
+                }
+                if edit || !currency.unitName.isEmpty {
+                    DetailFieldView(edit: edit, label: "Unit Name") {
+                        TextField("Unit Name", text: $currency.unitName)
+                    }
+                    if edit || !currency.centName.isEmpty {
+                        DetailFieldView(edit: edit, label: "Cent Name") {
+                            TextField("Cent Name", text: $currency.centName)
+                        }
+                    }
+                }
+                if edit {
+                    DetailFieldView(edit: edit, label: "Prefix Symbol") {
+                        TextField("Prefix Symbol", text: $currency.prefixSymbol)
+                    }
+                    DetailFieldView(edit: edit, label: "Suffix Symbol") {
+                        TextField("Suffix Symbol", text: $currency.suffixSymbol)
+                    }
+                    DetailFieldView(edit: edit, label: "Decimal Point") {
+                        TextField("Decimal Point", text: $currency.decimalPoint)
+                    }
+                    DetailFieldView(edit: edit, label: "Thousands Separator") {
+                        TextField("Thousands Separator", text: $currency.groupSeparator)
+                    }
+                    DetailFieldView(edit: edit, label: "Scale") {
+                        TextField("Scale", value: $currency.scale, format: .number)
+                    }
+                } else {
+                    DetailFieldView(edit: edit, label: "Format") {
+                        TextField("", text: $format)
+                    }
+                }
+                
+                /*
+                Section(header: Text("Conversion Rate")) {
+                    TextField("Conversion Rate", value: $currency.baseConvRate, format: .number)
+                }
+                Section(header: Text("Type")) {
+                    Picker("Currency Type", selection: $currency.type) {
+                        ForEach(CurrencyType.allCases) { type in
+                            Text(type.rawValue).tag(type)
+                        }
+                    }
+                    .labelsHidden()
+                    .pickerStyle(SegmentedPickerStyle()) // Adjust the style of the picker as needed
+                }
+
+                */
+
+                //DetailTextFieldView(edit: edit, label: "Conversion Rate", value: $currency.baseConvRate)
+                //DetailTextFieldView(edit: edit, label: "Type", value: $currency.type.rawValue)
+            }
+        }
+        .onAppear {
+            let amount: Double = 12345.67
+            format = amount.formatted(by: currency.formatter)
+        }
+    }
+    
+    var body2: some View {
         List {
             Section(header: Text("Name")) {
                 Text(currency.name)
