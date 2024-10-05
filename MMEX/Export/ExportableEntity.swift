2ff131462c56a797d3c5d9d246f344fb87de4a86
George Ef <george.a.ef@gmail.com>
2024-10-05 12:39:00 +0200

    rename folders

---
+++ MMEX/Export/ExportableEntity.swift
@@ -0,0 +1,32 @@
+//
+//  ExportableEntity.swift
+//  MMEX
+//
+//  Created by Lisheng Guan on 2024/9/13.
+//
+
+import Foundation
+import SwiftUI
+
+protocol ExportableEntity: Identifiable, Codable {
+    func export() -> String?
+    func copyToPasteboard()
+}
+
+extension ExportableEntity {
+    
+    // Default export as a JSON string
+    func export() -> String? {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        guard let data = try? encoder.encode(self) else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
+
+    // Copy the JSON string to pasteboard
+    func copyToPasteboard() {
+        if let exportString = self.export() {
+            UIPasteboard.general.string = exportString
+        }
+    }
+}
