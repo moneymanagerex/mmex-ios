8d06b0fba9c300621319bf6d797d22871405d620
George Ef <george.a.ef@gmail.com>
2024-11-23 23:57:14 +0100

    refactor Preference

---
+++ MMEX/Preference/StoredPreference.swift
@@ -0,0 +1,64 @@
+//
+//  StoredPreference.swift
+//  MMEX
+//
+//  2024-11-15: Created by George Ef (george.a.ef@gmail.com)
+//
+
+import Foundation
+
+@propertyWrapper
+struct StoredPreference<ValueType: LosslessStringConvertible> {
+    private var key: String?
+    private var value: ValueType
+    private let initValue: ValueType
+
+    var projectedValue: String? {
+        get { key }
+        set {
+            key = newValue
+            if let key, let stringValue = Self.load(key: key) {
+                value = ValueType(stringValue) ?? initValue
+            } else {
+                value = initValue
+            }
+        }
+    }
+
+    var wrappedValue: ValueType {
+        get { value }
+        set {
+            value = newValue
+            if let key {
+                Self.save(key: key, value: value.description)
+            }
+        }
+    }
+
+    init(key: String? = nil, wrappedValue initValue: ValueType) {
+        self.key = key
+        self.initValue = initValue
+        if let key, let stringValue = Self.load(key: key) {
+            value = ValueType(stringValue) ?? initValue
+        } else {
+            value = initValue
+        }
+    }
+
+    static func load(key: String) -> String? {
+        let value = UserDefaults.standard.string(forKey: key)
+        log.debug("DEBUG: Preference.load(\(key)): '\(value ?? "(nil)")')")
+        return value
+    }
+
+    static func save(key: String, value: String) {
+        log.debug("DEBUG: Preference.save(\(key), '\(value)')")
+        UserDefaults.standard.set(value, forKey: key)
+    }
+}
+
+extension StoredPreference where ValueType: EnumCollateNoCase {
+    init(key: String? = nil) {
+        self.init(key: key, wrappedValue: ValueType.defaultValue)
+    }
+}
---
+++ MMEX/Preference/Theme.swift
@@ -0,0 +1,46 @@
+//
+//  Theme.swift
+//  MMEX
+//
+//  2024-10-04: Created by George Ef (george.a.ef@gmail.com)
+//
+
+import Foundation
+import SwiftUI
+
+struct Theme {
+    @StoredPreference var appearance: Appearance = .defaultValue
+    @StoredPreference var numericKeypad: BoolEnum = .boolTrue
+    @StoredPreference var categoryDelimiter: String = ":"
+
+    var tab   : TabTheme   = .init()
+    var group : GroupTheme = .init()
+    var item  : ItemTheme  = .init()
+    var field : FieldTheme = .init()
+}
+
+extension Theme {
+    init(prefix: String?) {
+        self.$appearance        = prefix?.appending("appearance")
+        self.$numericKeypad     = prefix?.appending("numericKeypad")
+        self.$categoryDelimiter = prefix?.appending("categoryDelimiter")
+
+        self.tab   = .init(prefix: prefix?.appending("tab."))
+        self.group = .init(prefix: prefix?.appending("group."))
+        self.item  = .init(prefix: prefix?.appending("item."))
+        self.field = .init(prefix: prefix?.appending("field."))
+    }
+}
+
+extension Theme {
+    var decimalPad: UIKeyboardType {
+        switch numericKeypad {
+        case .boolTrue: .decimalPad
+        default: .alphabet
+        }
+    }
+
+    var textPad: UIKeyboardType {
+        .alphabet
+    }
+}
