7912ac452ee2cf6de3f83c6cc6158a6f334b78df
George Ef <george.a.ef@gmail.com>
2024-11-30 19:55:50 +0100

    add RepeatTypeNum.nextDate

--- MMEX/Data/ScheduledData.swift
+++ MMEX/Data/ScheduledData.swift
@@ -25,22 +25,22 @@ enum RepeatAuto: Int, CaseIterable, Identifiable, Codable {
 
 enum RepeatType: Int, CaseIterable, Identifiable, Codable {
     case once = 0
-    case weekly
-    case every2Weeks
-    case monthly
-    case every2Months
-    case every3Months
-    case every6Months
-    case yearly
-    case every4Months
-    case every4Weeks
-    case daily
-    case inXDays
-    case inXMonths
-    case everyXDays
-    case everyXMonths
-    case monthlyLastDay
-    case monthlyLastBusinessDay
+    case week1
+    case week2
+    case month1
+    case month2
+    case month3
+    case month6
+    case year1
+    case month4
+    case week4
+    case day1
+    case dayInX
+    case monthInX
+    case dayEveryX
+    case monthEveryX
+    case month1LastDay
+    case month1LastBusinessDay
     static let defaultValue = Self.once
     
     static let names = [
@@ -102,37 +102,94 @@ extension ScheduledData {
 
 enum RepeatTypeNum {
     case once
-    case times(RepeatType, PIntInf)
-    case inX(RepeatType, PInt)
-    case everyX(RepeatType, PInt)
+    case times  (RepeatType, PIntInf)
+    case inX    (RepeatType, PInt)
+    case everyX (RepeatType, PInt)
 }
 
 extension RepeatTypeNum {
-    var typeNum: (RepeatType, Int) {
+    var type: RepeatType {
         return switch self {
-        case .once             : (.once, -1)
-        case let .times(t, n)  : (t, n.value)
-        case let .inX(t, n)    : (t, n.value)
-        case let .everyX(t, n) : (t, n.value)
+        case     .once          : .once
+        case let .times  (t, _) : t
+        case let .inX    (t, _) : t
+        case let .everyX (t, _) : t
         }
     }
-
+    
+    var num: Int {
+        return switch self {
+        case     .once          : -1
+        case let .times  (_, n) : n.value
+        case let .inX    (_, n) : n.value
+        case let .everyX (_, n) : n.value
+        }
+    }
+    
     var times: PIntInf {
         return switch self {
-        case .once             : PIntInf(exactly: 1)!
-        case let .times(_, n)  : n
-        case .inX(_, _)        : PIntInf(exactly: 2)!
-        case .everyX(_, _)     : PIntInf.inf
+        case     .once          : PIntInf(exactly: 1)!
+        case let .times  (_, n) : n
+        case     .inX    (_, _) : PIntInf(exactly: 2)!
+        case     .everyX (_, _) : PIntInf.inf
+        }
+    }
+    
+    var next: Self? {
+        return switch self {
+        case     .once          : nil
+        case let .times  (t, n) : n.dec.map { .times(t, $0) }
+        case     .inX    (_, _) : .once
+        case let .everyX (t, n) : .everyX(t, n)
         }
     }
 
-    var next: Self? {
-        return switch self {
-        case .once             : nil
-        case let .times(t, n)  : n.dec.map { .times(t, $0) }
-        case .inX(_, _)        : .once
-        case let .everyX(t, n) : .everyX(t, n)
+    func nextDate(_ date: Date) -> Date? {
+        let cv: (Calendar.Component?, Int?) = switch type {
+        case .once                   : (nil, nil)
+        case .day1                  : (.day,   1)
+        case .dayInX                : (.day,   num)
+        case .dayEveryX             : (.day,   num)
+        case .week1                 : (.day,   1*7)
+        case .week2                 : (.day,   2*7)
+        case .week4                 : (.day,   4*7)
+        case .month1                : (.month, 1)
+        case .month1LastDay         : (.month, 1)
+        case .month1LastBusinessDay : (.month, 1)
+        case .month2                : (.month, 2)
+        case .month3                : (.month, 3)
+        case .month4                : (.month, 4)
+        case .month6                : (.month, 6)
+        case .monthInX              : (.month, num)
+        case .monthEveryX           : (.month, num)
+        case .year1                 : (.year,  1)
         }
+
+        guard let c = cv.0, let v = cv.1 else { return nil }
+        let calendar = Calendar(identifier: .gregorian)
+        var newDate = calendar.date(byAdding: c, value: v, to: date)!
+
+        if type == .month1LastDay || type == .month1LastBusinessDay {
+            // find the first day in next month and subtract one day
+            newDate = calendar.nextDate(
+                after: newDate, matching: DateComponents(day: 1),
+                matchingPolicy: .nextTime, direction: .forward
+            )!
+            newDate = calendar.date(byAdding: .day, value: -1, to: newDate)!
+        }
+
+        if type == .month1LastBusinessDay {
+            // if weekday is Sun or Sat, find the previous Fri
+            let d: Int = calendar.component(.weekday, from: newDate)
+            if d == 1 || d == 7 {
+                newDate = calendar.nextDate(
+                    after: newDate, matching: DateComponents(weekday: 6),
+                    matchingPolicy: .nextTime, direction: .backward
+                )!
+            }
+        }
+
+        return newDate
     }
 }
 
@@ -140,10 +197,10 @@ extension ScheduledData {
     var repeatTypeNum: RepeatTypeNum? {
         switch repeatType {
         case .once: return .once
-        case .inXDays, .inXMonths:
+        case .dayInX, .monthInX:
             let n: PInt? = PInt(exactly: repeatNum)
             return n.map { .inX(repeatType, $0) }
-        case .everyXDays, .everyXMonths:
+        case .dayEveryX, .monthEveryX:
             let n: PInt? = PInt(exactly: repeatNum)
             return n.map { .everyX(repeatType, $0) }
         default:
